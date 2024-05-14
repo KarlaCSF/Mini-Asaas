0e538eb378e0b0b193031eb90477a573815da3c8
Karla Feliciano <78981063+KarlaCSF@users.noreply.github.com>
2024-05-14 11:03:11 -0300

    refactor: remove nullable false e separa propriedades

--- src/main/groovy/utils/PersonEntity.groovy
+++ src/main/groovy/utils/PersonEntity.groovy
@@ -4,17 +4,18 @@ import com.mini.asaas.PersonType
 
 abstract class PersonEntity extends BaseEntity {
     String name
+    
     String email
+    
     PersonType personType
+    
     String cpfCnpj
+    
     Address address
 
     static constraints = {
-      name nullable: false
-      email nullable: false, email: true
-      personType nullable: true
-      cpfCnpj nullable: false, minSize: 11, maxSize: 14
-      address nullable: true
+      email email: true
+      cpfCnpj minSize: 11, maxSize: 14
     }
 
     static mapping = {
