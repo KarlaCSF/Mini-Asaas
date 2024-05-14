b6cadfedd5eed8d01403dc8842184a899eb9e665
SAVANOo <savano947@gmail.com>
2024-05-14 15:39:28 -0300

    Add: Adicionando log de erro nas exceptions

--- grails-app/controllers/com/mini/asaas/PayerController.groovy
+++ grails-app/controllers/com/mini/asaas/PayerController.groovy
@@ -5,7 +5,12 @@ class PayerController {
 
     PayerService payerService
 
+    def index() {
+        render(view: "index")
+    }
+
     def save() {
+        log.info("Starting the save the payer")
         try {
             String name = params.name
             String email = params.email
@@ -14,17 +19,21 @@ class PayerController {
             redirect(action: "show", id: payer.id)
         } catch (Exception exception) {
             redirect(action: "index", params: params)
+            log.error("Error in save payer:", exception)
         }
     }
 
     def show() {
+        log.info("Starting to show the payer")
         try {
             Payer payer = Payer.get(params.id)
             if (!payer) {
+                log.error("Error in catch payer:", exception)
                 render "Pagador não encontrado"
             }
             return [payer: payer]
         } catch (Exception exception) {
+            log.error("Error in catch payer:", exception)
             render "Pagador não encontrado"
         }
     }
