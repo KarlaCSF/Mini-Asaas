d7db6d318d7f6884983108daa6e5a947c9542b31
Sávio Mendes <91269138+SAVANOo@users.noreply.github.com>
2024-05-14 11:00:58 -0300

    Fix: removendo ponto e virgula do código
    Co-authored-by: Arthur Nascimento <62653479+TuiNascimento@users.noreply.github.com>

--- grails-app/services/PayerService.groovy
+++ grails-app/services/PayerService.groovy
@@ -6,13 +6,13 @@ import com.mini.asaas.Payer
 @Transactional
 class PayerService {
     public Payer save(String name, String email, String cpfCnpj, String address, String password) {
-        Payer payer = new Payer();
-        payer.name = name;
-        payer.email = email;
-        payer.cpfCnpj = cpfCnpj;
-        payer.address = address;
-        payer.password =password;
-        payer.save(failOnError: true);
-        return payer;
+        Payer payer = new Payer()
+        payer.name = name
+        payer.email = email
+        payer.cpfCnpj = cpfCnpj
+        payer.address = address
+        payer.password =password
+        
+        return payer.save(failOnError: true)
     }
 }
