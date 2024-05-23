fa90517af88508ef85df950670df5044dda86941
SAVANOo <savano947@gmail.com>
2024-05-23 13:18:53 -0300

    feat: messages of enums is per language of user

--- src/main/groovy/com/mini/asaas/enums/payment/BillingType.groovy
+++ src/main/groovy/com/mini/asaas/enums/payment/BillingType.groovy
@@ -2,9 +2,9 @@ package com.mini.asaas.enums.payment
 
 import grails.util.Holders
 import java.util.Locale
+import org.springframework.web.context.request.RequestContextHolder
 
 enum BillingType {
-    
     BANK_SLIP,
     CREDIT,
     DEBIT,
@@ -12,7 +12,8 @@ enum BillingType {
     PIX
     
     String getMessage() {
+        Locale locale = RequestContextHolder.currentRequestAttributes().getLocale()
         def messageSource = Holders.applicationContext.getBean("messageSource")
-        messageSource.getMessage("BillingType.${this.name()}", null, "", new Locale("pt", "BR"))
+        messageSource.getMessage("BillingType.${this.name()}", null, "", locale)
     }
 }
