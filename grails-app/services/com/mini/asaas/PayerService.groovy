1d6cba8d760c015c7f4c00ec3e214f976d7a1e71
Karla Feliciano <karla.s.feliciano@gmail.com>
2024-05-27 13:15:22 -0300

    fix: add validation to payer

--- grails-app/services/com/mini/asaas/PayerService.groovy
+++ grails-app/services/com/mini/asaas/PayerService.groovy
@@ -8,6 +8,7 @@ import com.mini.asaas.dto.PayerDTO
 import com.mini.asaas.utils.CpfCnpjUtils
 
 import javax.transaction.Transactional
+import grails.validation.ValidationException
 import grails.compiler.GrailsCompileStatic
 
 @GrailsCompileStatic
@@ -17,16 +18,28 @@ class PayerService {
     AddressService addressService
 
     public Payer save(PayerDTO payerDTO, Long customerId) {
-        Payer payer = new Payer()
+        Payer payer = validateSave(payerDTO)
+
+        if (payer.hasErrors()) throw new ValidationException("Erro ao salvar conta", payer.errors)
 
         payer.name = payerDTO.name
         payer.email = payerDTO.email
         payer.cpfCnpj = payerDTO.cpfCnpj
         payer.customer = Customer.get(customerId)
-        payer.personType = CpfCnpjUtils.setPersonType(payer.cpfCnpj)
+        payer.personType = CpfCnpjUtils.getPersonType(payer.cpfCnpj)
         
         payer.address = addressService.save(payerDTO.addressDTO)
         
         return payer.save(failOnError: true)
+    }
+
+    private Payer validateSave(PayerDTO payerDTO) {
+        Payer payer = new Payer()
+        
+        if (!CpfCnpjUtils.validate(payerDTO.cpfCnpj)) {
+            payer.errors.reject("cpfCnpj", null, "CPF ou CNPJ inválido.")
+        }
+        
+        return payer
     }
 }
