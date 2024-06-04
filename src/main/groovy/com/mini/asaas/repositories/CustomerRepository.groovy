4b6f785c6c3679e63900822d6eb41eda4d1c1d9a
SAVANOo <savano947@gmail.com>
2024-06-04 14:19:12 -0300

    feat (CustomerRepository) : default query to Customer

---
+++ src/main/groovy/com/mini/asaas/repositories/CustomerRepository.groovy
@@ -0,0 +1,13 @@
+package com.mini.asaas.repositories
+
+import com.mini.asaas.Customer
+import com.mini.asaas.repositories.Repository
+import grails.gorm.DetachedCriteria
+
+class CustomerRepository implements Repository {
+
+    public static DetachedCriteria<Customer> query(Map search) {
+        DetachedCriteria<Customer> query = Customer.where(defaultQuery(search))    
+        return query
+    }
+}
