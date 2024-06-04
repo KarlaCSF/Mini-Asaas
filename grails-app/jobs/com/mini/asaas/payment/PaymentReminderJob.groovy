28c16cd757e5c9f86d3e7cf8b1876a82367ee7f2
SAVANOo <savano947@gmail.com>
2024-06-04 10:16:13 -0300

    remove (PaymentReminderJob) : unnecessary annotation

--- grails-app/jobs/com/mini/asaas/payment/PaymentReminderJob.groovy
+++ grails-app/jobs/com/mini/asaas/payment/PaymentReminderJob.groovy
@@ -1,6 +1,5 @@
 package com.mini.asaas.payment
 
-import grails.gorm.transactions.Transactional
 import com.mini.asaas.enums.payment.PaymentStatus
 import com.mini.asaas.email.EmailService
 
@@ -12,7 +11,6 @@ class PaymentReminderJob {
     cron name: 'PaymentReminderJobTrigger', cronExpression: "0 0 7 * * ?", startDelay: 10000
   }
 
-  @Transactional
   def execute() {
     emailService.fetchWaitingPaymentsAndRemindPayer()
   }
