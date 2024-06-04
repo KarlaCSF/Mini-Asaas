eacde45f781bace01f857e446d118f88d7d9044e
SAVANOo <savano947@gmail.com>
2024-06-04 14:40:44 -0300

    feat (CustomerRepository) : add a method to find customer by id

--- src/main/groovy/com/mini/asaas/repositories/CustomerRepository.groovy
+++ src/main/groovy/com/mini/asaas/repositories/CustomerRepository.groovy
@@ -2,6 +2,7 @@ package com.mini.asaas.repositories
 
 import com.mini.asaas.customer.Customer
 import com.mini.asaas.repositories.Repository
+
 import grails.gorm.DetachedCriteria
 
 class CustomerRepository implements Repository {
@@ -9,5 +10,11 @@ class CustomerRepository implements Repository {
     public static DetachedCriteria<Customer> query(Map search) {
         DetachedCriteria<Customer> query = Customer.where(defaultQuery(search))    
         return query
+    }
+    
+    public Customer findById(Long customerId) {
+        Customer customer = CustomerRepository.query([id: customerId]).get()
+        if (!customer) throw new Exception("Customer inexistente.")
+        return customer
     }
 }
