b950e887e617ae5206a2a4383fb54c4727a085c2
Karla Feliciano <karla.s.feliciano@gmail.com>
2024-06-07 19:03:56 -0300

    wip: create roles and test users

--- grails-app/init/com/mini/asaas/BootStrap.groovy
+++ grails-app/init/com/mini/asaas/BootStrap.groovy
@@ -4,15 +4,39 @@ import com.mini.asaas.User
 import com.mini.asaas.Role
 import com.mini.asaas.UserRole
 
+import grails.gorm.transactions.Transactional
+
 class BootStrap {
-    def init = {
+def init = {
+        addTestUser()
+    }
+
+    @Transactional
+    void addTestUser() {
+        def adminRole
         if (!Role.findByAuthority('ROLE_ADMIN')) {
-            new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+            adminRole = new Role(authority: 'ROLE_ADMIN')
+            adminRole.save(failOnError: true)
         }
 
         if (!Role.findByAuthority('ROLE_SELLER')) {
             new Role(authority: 'ROLE_SELLER').save(failOnError: true)
         }
+
+        if(!User.findByUsername('me')) {
+            def testUser = new User(username: 'me', password: 'password')
+            testUser.save(failOnError: true)
+            UserRole.create(testUser, adminRole, true)
+        }
+
+        UserRole.withSession {
+            it.flush()
+            it.clear()
+        }
+
+        assert User.count() >= 1
+        assert Role.count() >= 1
+        assert UserRole.count() == 1
     }
     
     def destroy = {
