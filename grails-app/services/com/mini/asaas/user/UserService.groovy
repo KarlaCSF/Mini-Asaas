e780ad4afabaf67fa7e3c5a762fecb2dee12ba04
Karla Feliciano <karla.s.feliciano@gmail.com>
2024-06-11 13:26:54 -0300

    fix: remove unecessary

--- grails-app/services/com/mini/asaas/user/UserService.groovy
+++ grails-app/services/com/mini/asaas/user/UserService.groovy
@@ -3,15 +3,9 @@ package com.mini.asaas.user
 import grails.gorm.transactions.Transactional
 import grails.compiler.GrailsCompileStatic
 
-import grails.gorm.services.Service
-import grails.plugin.springsecurity.SpringSecurityService
-
 @GrailsCompileStatic
 @Transactional
 class UserService {
-
-    SpringSecurityService springSecurityService
-
     User save(String username, String password, Role role) {
         def user = new User(username: username, password: password)
         user.save(failOnError: true)
