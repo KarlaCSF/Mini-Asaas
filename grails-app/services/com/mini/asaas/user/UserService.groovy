9f7bfa742b5da49c61d767b4dc497160f361c772
Karla Feliciano <karla.s.feliciano@gmail.com>
2024-06-11 16:19:25 -0300

    feat: functions to get user and related customer

--- grails-app/services/com/mini/asaas/user/UserService.groovy
+++ grails-app/services/com/mini/asaas/user/UserService.groovy
@@ -2,10 +2,14 @@ package com.mini.asaas.user
 
 import grails.gorm.transactions.Transactional
 import grails.compiler.GrailsCompileStatic
+import grails.plugin.springsecurity.SpringSecurityService
+import com.mini.asaas.customer.Customer
 
 @GrailsCompileStatic
 @Transactional
 class UserService {
+    SpringSecurityService springSecurityService
+
     User save(String username, String password, Role role) {
         def user = new User(username: username, password: password)
         user.save(failOnError: true)
@@ -13,5 +17,14 @@ class UserService {
         UserRole.create(user, role)
 
         return user
+    }
+
+    public User getCurrentUser() {
+        return (User) springSecurityService.getCurrentUser()
+    }
+
+    public Customer getCustomerByUser() {
+        User user = (User) springSecurityService.getCurrentUser()
+        return user.customer
     }
 }
