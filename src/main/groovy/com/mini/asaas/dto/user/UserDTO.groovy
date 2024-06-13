c5a557adf92c301078888a9c0977eefa46ccd57b
Karla Feliciano <karla.s.feliciano@gmail.com>
2024-06-13 14:10:59 -0300

    feat: edit user

--- src/main/groovy/com/mini/asaas/dto/user/UserDTO.groovy
+++ src/main/groovy/com/mini/asaas/dto/user/UserDTO.groovy
@@ -2,6 +2,7 @@ package com.mini.asaas.dto.user
 
 import grails.compiler.GrailsCompileStatic
 import com.mini.asaas.customer.Customer
+import com.mini.asaas.user.Role
 
 @GrailsCompileStatic
 class UserDTO {
@@ -12,9 +13,12 @@ class UserDTO {
 
     Customer customer
 
+    Role role
+
     public UserDTO(Map params){
         this.username = params.email
         this.password = params.password
         this.customer = (Customer) params.customer
+        this.role = (Role) params.role
     } 
 }
