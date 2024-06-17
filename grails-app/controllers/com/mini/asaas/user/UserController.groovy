5c0eb72fcfc3e98982c8ca907ce17bfdfc2661e6
Karla Feliciano <karla.s.feliciano@gmail.com>
2024-06-17 14:07:33 -0300

    refactor: improve log message

--- grails-app/controllers/com/mini/asaas/user/UserController.groovy
+++ grails-app/controllers/com/mini/asaas/user/UserController.groovy
@@ -20,25 +20,27 @@ class UserController {
     EmailService emailService
 
     def users() {
+        Long customerId
         try {
-            Customer customer = userService.getCustomerByUser()
-            List<User> userList = UserRepository.listByCustomer(customer.id)
+            customerId = userService.getCurrentCustomerIdForLoggedUser()
+            List<User> userList = UserRepository.listByCustomer(customerId)
             List<Role> roleList = RoleRepository.listAll()
             return [userList: userList, roleList: roleList]
         } catch (Exception exception) {
-            log.error("UserController.users >> Não foi possível listar os Users", exception)
+            log.error("UserController.users >> Não foi possível listar os Users do Customer ${customerId}", exception)
             params.errorMessage = "Não foi possível listar os usuários"
             redirect(view: "edit", params: params)
         }
     }
 
     def add() {
+        Customer customer
         try {
             Random randomPassword = new Random()
-            int maxLenght = 999999
-            params.password = randomPassword.nextInt(maxLenght)
+            int maxLength = 999999
+            params.password = randomPassword.nextInt(maxLength)
 
-            Customer customer = userService.getCustomerByUser()
+            customer = userService.getCustomerByUser()
             params.customer = customer
  
             params.role = Role.findByAuthority(params.role)
@@ -50,31 +52,33 @@ class UserController {
 
             redirect(action: 'users')
         } catch (Exception exception) {
-            log.error("UserController.add >> Não foi possível adicionar o User", exception)
+            log.error("UserController.add >> Não foi possível adicionar o User no Customer ${customer.id}", exception)
             params.errorMessage = "Não foi possível adicionar o usuário"
             redirect(action: 'users', params: params)
         }
     }
 
     def edit() {
+        User user
         try {
-            User user = userService.getCurrentUser()
+            user = userService.getCurrentUser()
             return [user: user]
         } catch (Exception exception) {
-            log.error("UserController.edit >> Não foi possível buscar o User", exception)
+            log.error("UserController.edit >> Não foi possível buscar o User ${user.id}", exception)
             params.errorMessage = "Não foi possível buscar o usuário"
             redirect(action: "users", params: params)
         }
     }
 
     def update() {
+        User user
         try {
-            User user = userService.getCurrentUser()
+            user = userService.getCurrentUser()
             UserDTO userDTO = new UserDTO(params)
             userService.update(userDTO, user)
             redirect(action: 'users')
         } catch (Exception exception) {
-            log.error("UserController.update >> Não foi possível atualizar o usuário", exception)
+            log.error("UserController.update >> Não foi possível atualizar o User ${user.id}", exception)
             params.errorMessage = "Não foi possível atualizar o usuário"
             redirect(action: 'edit', params: params)
         }
